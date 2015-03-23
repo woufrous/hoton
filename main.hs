22e11e4699bffb1b9b11609d71d1439bd675f932
Hans Grob <H.Grob@physik.uni-muenchen.de>
2015-03-23 13:58:21 +0100

    Switched from tabs to spaces for indention

--- main.hs
+++ main.hs
@@ -3,19 +3,19 @@ import System.Environment
 
 main :: IO ()
 main = do
-	args <- getArgs
-	if length args /= 1
-	then do
-		return ()
-	else do
-		let numSamples = read $ head args :: Int
-		gen <- getStdGen
-		let rs = take numSamples $ randomRs (0,1) gen :: [Double]
-		mapM_ print (map cardano rs)
+    args <- getArgs
+    if length args /= 1
+    then do
+        return ()
+    else do
+        let numSamples = read $ head args :: Int
+        gen <- getStdGen
+        let rs = take numSamples $ randomRs (0,1) gen :: [Double]
+        mapM_ print (map cardano rs)
 
 cardano :: Double -> Double
 cardano r = u - (1/u)
-	where
-		u = (-(q/2) + sqrt d)**(1/3 :: Double)
-		d = 1 + (q**2 / 4)
-		q = -8*r + 4
+    where
+        u = (-(q/2) + sqrt d)**(1/3 :: Double)
+        d = 1 + (q**2 / 4)
+        q = -8*r + 4
