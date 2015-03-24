a9ba29d0c0372dc96549bf2b4e43c8259a3a68e2
Hans Grob <H.Grob@physik.uni-muenchen.de>
2015-03-24 11:37:37 +0100

    RandomDistribution: code cleanup
    Members of the RandomDistribution typeclass now only need to implement a sample
    fuction that takes a random number. The entire random number generation shit is
    impemented in the typeclass.

--- Hoton/Distributions.hs
+++ Hoton/Distributions.hs
@@ -9,43 +9,40 @@ module Hoton.Distributions
 import System.Random
 
 class RandomDistribution rd where
-    drawRandom  :: RandomGen g => rd -> g -> (Double, g)
-    drawRandoms :: (RandomGen g, RandomDistribution rd, Integral n) => rd -> g -> n -> ([Double], g)
+    sample :: rd -> Double -> Double
+    drawRandom  :: (RandomGen g) => rd -> g -> (Double, g)
+    drawRandom rdf g = (sample rdf r, g')
+        where
+            (r, g') = randomR (0,1) g
+    drawRandoms :: (RandomGen g, Integral n) => rd -> g -> n -> ([Double], g)
     drawRandoms _ g 0 = ([], g)
     drawRandoms rdf g n = (x:xs, ng')
         where
             (x, ng) = drawRandom rdf g
             (xs, ng') = drawRandoms rdf ng (n-1)
 
-trans :: RandomGen g => (Double -> Double) -> g -> (Double, g)
-trans f g = (f r, g')
-    where
-        (r, g') = randomR (0,1) g
-
 data Rayleigh = Rayleigh
 instance RandomDistribution Rayleigh where
-    drawRandom Rayleigh = trans (\r -> let
+    sample Rayleigh r = u - (1/u)
+        where
             u = (-(q/2) + sqrt d)**(1/3 :: Double)
             d = 1 + (q**2 / 4)
             q = -8*r + 4
-            in
-            u - (1/u))
 
 
 data HenyeyGreenstein = HenyeyGreenstein Double
 instance RandomDistribution HenyeyGreenstein where
-    drawRandom (HenyeyGreenstein assym) g = (u/(2*assym), g')
+    sample (HenyeyGreenstein assym) r = u/(2*assym)
         where
-            (r, g') = randomR (0,1) g
             u = -v**2 + assym**2 + 1
             v = (1-assym**2)/w
             w = assym * (2*r - 1) + 1
 
 data ThicknessDistribution = ThicknessDistribution
 instance RandomDistribution ThicknessDistribution where
-    drawRandom ThicknessDistribution = trans (\r -> (- log (1 - r)))
+    sample ThicknessDistribution r = - log (1 - r)
 
 data LambertDistribution = LambertDistribution
 instance RandomDistribution LambertDistribution where
-    drawRandom LambertDistribution = trans sqrt
+    sample LambertDistribution = sqrt
 
