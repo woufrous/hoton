3d18231adf782df9edca151f809ae5a008cfc1cb
Tobias Kölling <tobi@die70.de>
2015-03-25 00:12:46 +0100

    added debug helper

---
+++ Hoton/Debug.hs
@@ -0,0 +1,10 @@
+import Debug.Trace
+
+_traceShowId a = traceShow a a
+
+_breakIfVectorIsNan :: Cartesian -> Cartesian
+_breakIfVectorIsNan (Cartesian x1 x2 x3)
+    | isNaN x1 || isNaN x2 || isNaN x3    = error("NAN reached")
+    | x1 == 0.0 && x2 == 0.0 && x3 == 0.0 = error("0.0 reached")
+    | otherwise                           = Cartesian x1 x2 x3
+
